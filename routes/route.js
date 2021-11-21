e15713446ac01a9de6a1ad082ab88c3bacee51cd
leewoobi <79895996+leewoobi@users.noreply.github.com>
2021-11-22 00:06:42 +0900

    add  crud

--- routes/route.js
+++ routes/route.js
@@ -2,19 +2,37 @@ var router = require('express').Router();
 
 
 
-router.get('/',(request, response)=>{
-    response.render('index.ejs')
+router.get('/',(req,res)=>{
+    res.render('index.ejs')
  });
  
- router.get('/write',(request, response)=>{
-     response.render('write.ejs');
+ router.get('/write',(req,res)=>{
+    res.render('write.ejs');
   });
+
+ router.get('/edit/:id',(req,res)=>{
+   req.app.db.collection('posts').findOne({_id : parseInt(req.params.id)},(error,result)=>{
+
+    res.render('edit.ejs', {data : result});
+   });
  
-  router.get('/list', (request, response)=>{
-    request.app.db.collection('posts').find().toArray((error,result)=>{
-         console.log(result);
-         response.render('list.ejs', {posts: result});
+});
+ 
+  router.get('/list', (req,res)=>{
+    req.app.db.collection('posts').find().toArray((error,result)=>{
+        //  console.log(result);
+        res.render('list.ejs', {posts: result});
      });
  });
 
+
+  router.get('/detail/:id',(req,res)=>{
+    req.app.db.collection('posts').findOne({_id :parseInt(req.params.id)},(error,result)=>{
+          console.log(result);
+          res.render('detail.ejs',{ data : result});
+      })
+
+  })
+
+ 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config();
-
+const methodOverride = require('method-override');
+require('dotenv').config(); // 환경변수
 var db;
+
 
 MongoClient.connect(process.env.DB_URL, { useUnifiedTopology: true },  (error, client)=>{
     if(error) return console.log(error)
@@ -16,19 +17,45 @@ MongoClient.connect(process.env.DB_URL, { useUnifiedTopology: true },  (error, c
     });
 });
 
-app.use(bodyParser.urlencoded({extended : true }));
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
-app.set('view engine', 'ejs');
-app.use('/',require('./routes/route.js'));
+app.use(methodOverride('_method'))
+app.use('/public',express.static('public'));
+app.use(bodyParser.urlencoded({extended : true })); //bodyParser
+// app.use('/node_modules', express.static(path.join(__dirname + '/node_modules/bootstrap/dist/js'))); // redirect bootstrap JS
+// app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.set('view engine', 'ejs'); //ejs 
+app.use('/',require('./routes/route.js')); // routes 
 
 
 
- app.post('/add',(request, response)=>{
-     response.send('전송완료');
-     console.log(request.body);
-
-     db.collection('posts').insertOne({title: request.body.title , date: request.body.date}, (error, result)=>{
-         console.log('저장완료');
+ app.post('/add',(req, res)=>{
+    //  response.send('전송완료');
+    res.redirect('/list');
+     db.collection('counter').findOne({name: 'postNum' }, (error, result)=>{
+         console.log(result.totalPost)
+         var totalPostNum = result.totalPost;
+         db.collection('posts').insertOne({_id:totalPostNum + 1, title: req.body.title , date: req.body.date}, (error, result)=>{
+         db.collection('counter').updateOne({name:'postNum'},{ $inc : {totalPost:1}}, (error,result)=>{
+                if(error){return console.log(error)}
+            });
+        console.log('저장완료');
+        });
      });
  });
+
+ 
+
+ app.delete('/delete',(req,res)=>{
+    console.log(req.body);
+    req.body._id  = parseInt(req.body._id);  
+    db.collection('posts').deleteOne(req.body, (error,result)=>{
+        console.log('삭제완료')
+        res.status(200).send({message:'성공했습니다'});
+    }  )
+})
+
+app.put('/edit', (req,res)=>{
+    db.collection('posts').updateOne({_id : parseInt(req.body.id) },{ $set : { title:req.body.title, date: req.body.date }},(error, result)=>{
+        console.log('수정완료')
+        res.redirect('/list')
+    })
+})
