c2c3cf74c0f8a9fd9edbbb9fccfc030398c8ef2f
leewoobi <79895996+leewoobi@users.noreply.github.com>
2021-11-24 00:22:40 +0900

    encryption(암호화) & refactoring

--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,6 @@
 var router = require('express').Router();
 
 router.get('/',(req,res)=>{
-   console.log(req.user);
     res.render('index.ejs',{user : req.user})
  });
 
@@ -9,68 +8,46 @@ router.get('/write',(req,res)=>{
     res.render('write.ejs',{user : req.user});
   });
 
-  
 router.get('/login',(req,res)=>{
     res.render('login.ejs',{user : req.user});
-  });
-
-router.get('/mypage',loginCheck, (req,res)=>{
-    res.render('mypage.ejs', {user : req.user});
   });
 
 router.get('/signup',(req,res)=>{
     res.render('signUp.ejs',{user : req.user});
   });
 
-
  router.get('/edit/:id',(req,res)=>{
    req.app.db.collection('posts').findOne({_id : parseInt(req.params.id)},(error,result)=>{
-    res.render('edit.ejs', {data : result});
+    res.render('edit.ejs', {data : result, user : req.user});
    });
  
 });
 
-// router.get('/list',(req,res)=>{
-
-//   var result = req.app.db.collection('posts').find().toArray((error,result)=>  result )
-  
-//   res.render('list.ejs', {posts: result},{user: req.user});
-// });
-
-
-  router.get('/list',(req,res)=>{
-    req.app.db.collection('posts').find().toArray((error,result)=>{
-        res.render('list.ejs', {posts: result},{user: req.user});
-     });
+router.get('/list',(req,res)=>{
+  req.app.db.collection('posts').find().toArray((error,result)=>{
+    res.render('list.ejs', {posts: result, user: req.user});
  });
+})
 
- 
-
-
-  router.get('/detail/:id', (req,res)=>{
-    req.app.db.collection('posts').findOne({_id :parseInt(req.params.id)},(error,result)=>{
-          console.log(result);
-          res.render('detail.ejs',{ data : result},{user : req.user});
-      })
-
+router.get('/search',(req,res)=>{
+  var seachCon = [
+    {
+      $search: {
+        index: 'titleSearch',
+        text: {
+          query: req.query.value,
+          path: 'title'  // 제목, 날짜 둘다 찾고 싶으면 ['title', 'date']
+        }
+      }
+    },
+    {$sort : {_id : 1}},
+    // {$limit : 10} 갯수 제한
+  ] 
+  req.app.db.collection('posts').aggregate(seachCon).toArray((error,result)=>{
+    console.log(result);
+    res.render('searchRes.ejs', {posts: result , user: req.user});
   })
-
-
-  function loginCheck(req,res,next){
-    if(req.user){
-      next()
-    } else{
-      res.send('로그인 하세요')
-    }
-  }
-  
-  
-
-  router.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
-  });
-  
+})
   
  
 module.exports = router;
--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const methodOverride = require('method-override');
 var flash = require('connect-flash');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
-
+const passport = require('passport');  //로그인
+const LocalStrategy = require('passport-local').Strategy; //세션 
+const session = require('express-session'); //세션 
+const crypto = require('crypto'); // 암호화 
 require('dotenv').config(); // 환경변수
 var db;
 
@@ -45,13 +45,14 @@ passport.use(new LocalStrategy({
     session: true,
     passReqToCallback: false,
   }, function (inputId, inputPw, done) {
-  
-    console.log(inputId, inputPw);
+    const inputPassword = inputPw;
     db.collection('member').findOne({ id: inputId }, function (error, result) {
+      const salt = result.salt;
+      const hashPassword = crypto.createHash('sha512').update(inputPassword + salt).digest('hex');
       if (error) return done(error)
   
       if (!result) return done(null, false, { message: '존재하지않는 아이디요' })
-      if (inputPw == result.pw) { //암호화 필요 
+      if (hashPassword == result.pw ) { 
         return done(null, result)
       } else {
         return done(null, false, { message: '비번틀렸어요' })
@@ -71,6 +72,10 @@ passport.deserializeUser((id,done)=>{
       done(null,result)
    });
 });
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
 
 
 app.post('/login', passport.authenticate('local', {
@@ -81,31 +86,6 @@ app.post('/login', passport.authenticate('local', {
  );
 
 
- app.post('/add',(req, res)=>{
-    //  response.send('전송완료');
-    res.redirect('/list');
-     db.collection('counter').findOne({name: 'postNum' }, (error, result)=>{
-         console.log(result.totalPost)
-         var totalPostNum = result.totalPost;
-         db.collection('posts').insertOne({_id:totalPostNum + 1, title: req.body.title , date: req.body.date}, (error, result)=>{
-         db.collection('counter').updateOne({name:'postNum'},{ $inc : {totalPost:1}}, (error,result)=>{
-                if(error){return console.log(error)}
-            });
-        console.log('저장완료');
-        });
-     });
- });
-
- 
-
- app.delete('/delete',(req,res)=>{
-    console.log(req.body);
-    req.body._id  = parseInt(req.body._id);  
-    db.collection('posts').deleteOne(req.body, (error,result)=>{
-        console.log('삭제완료')
-        res.status(200).send({message:'성공했습니다'});
-    }  )
-})
 
 app.put('/edit', (req,res)=>{
     db.collection('posts').updateOne({_id : parseInt(req.body.id) },{ $set : { title:req.body.title, date: req.body.date }},(error, result)=>{
@@ -114,19 +94,46 @@ app.put('/edit', (req,res)=>{
     })
 })
 
-// app.post('/login', passport.authenticate('local',{
-//     failureRedirect : '/fail'
-// }), (req,res)=>{
-//     res.redirect('/')
-// });
-
 
 
 app.post('/signup', (req,res)=>{
-    db.collection('member').insertOne({id: req.body.id, name: req.body.name , pw:req.body.pw, email: req.body.email} ,(error,result)=>{
+    const inputPassword = req.body.pw;
+    const salt = crypto.randomBytes(128).toString('base64');
+    const hashPassword = crypto.createHash('sha512').update(inputPassword + salt).digest('hex');
+
+    db.collection('member').insertOne({id: req.body.id, name: req.body.name ,salt: salt, pw:hashPassword, email: req.body.email} ,(error,result)=>{
     res.redirect('login');
     console.log('singUp save');
 
     })
 })
 
+app.post('/add',(req, res)=>{
+  //  response.send('전송완료');
+  res.redirect('/list');
+   db.collection('counter').findOne({name: 'postNum' }, (error, result)=>{
+       console.log(result.totalPost)
+       var totalPostNum = result.totalPost;
+
+       var saveInfo = {_id:totalPostNum + 1, title: req.body.title , date: req.body.date, writer: req.user._id }
+       db.collection('posts').insertOne(saveInfo, (error, result)=>{
+       db.collection('counter').updateOne({name:'postNum'},{ $inc : {totalPost:1}}, (error,result)=>{
+              if(error){return console.log(error)}
+          });
+      console.log('저장완료');
+      });
+   });
+});
+
+app.delete('/delete',(req,res)=>{
+  console.log(req.body);
+  req.body._id  = parseInt(req.body._id);  
+
+  var deleteData = {_id : req.body._id , writer : req.user._id }
+  
+  db.collection('posts').deleteOne(deleteData, (error,result)=>{
+      console.log('삭제완료')
+      if(error) {console.log(error)}
+      res.status(200).send({message:'성공했습니다'});
+  }  )
+})
